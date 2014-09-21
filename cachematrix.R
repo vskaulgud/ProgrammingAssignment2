451384c77b9678957f686a265bb609b52acfa344
vskaulgud <vskaulgud@gmail.com>
2014-09-21 12:48:36 -0700

    edited cache matrix to implement the functionality

--- cachematrix.R
+++ cachematrix.R
@@ -1,16 +1,35 @@
-## Put comments here that give an overall description of what your
-## functions do
-hjh
+#makeCacheMatrix inputs a square matrix and returns a list of four function objects
+#makeCacheMatrix inputs the list created by makeCacheMatrix and returns the inverse of the matrix
 
-## Write a short comment describing this function
-
+## Returns a list with four function objects for a given matrix
 makeCacheMatrix <- function(x = matrix()) {
+	inv <- NULL
+	set <- function(y){
+		x<<-y
+		inv <<- NULL
+	}
+	get <- function() x
+	getinv <- function() inv
+	setinv <- function(inverse) inv <<- inverse
+	
+	list(set = set, get = get, setinv = setinv, getinv = getinv)
 
 }
 
 
-## Write a short comment describing this function
+## Returns the Inverse matrix from the list created by the function makeCacheMatrix
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        m <- x$getinv()
+	if(!is.null(m)){
+		print('Getting cached copy for the Inverse')
+		return(m)
+	}
+	
+	data <- x$get()
+	m <- solve(data)
+	x$setinv(m)
+	m
+
 }
